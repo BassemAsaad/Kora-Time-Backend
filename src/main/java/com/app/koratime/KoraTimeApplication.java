6f16997a4d5328e696381eb93ed6272807b9366f
BassemAsaad <bassemasaad11@gmail.com>
2026-03-28 06:27:30 +0200

    setup

---
+++ src/main/java/com/app/koratime/KoraTimeApplication.java
@@ -0,0 +1,13 @@
+package com.app.koratime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoraTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoraTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/koratime/KoraTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.koratime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoraTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
