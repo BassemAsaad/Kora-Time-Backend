d56396a79aca3c2eab31526b24b239a6e1b4851a
BassemAsaad <bassemasaad11@gmail.com>
2026-06-04 20:56:01 +0300

    feat(user) add factory to create user and profiles

--- src/main/java/com/app/koratime/user/factory/UserFactory.java
+++ src/main/java/com/app/koratime/user/factory/UserFactory.java
@@ -1,4 +1,40 @@
 package com.app.koratime.user.factory;
 
-public class UserFactory {
+import com.app.koratime.user.model.*;
+
+public final class UserFactory {
+
+    private UserFactory() {}
+
+    public static User buildUser(
+            String email, String password, String firstName, String lastName, String phoneNumber, UserRole role) {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .firstName(firstName)
+                .lastName(lastName)
+                .phoneNumber(phoneNumber)
+                .role(role)
+                .build();
+    }
+
+    public static PlayerProfile buildPlayerProfile(User user) {
+        return PlayerProfile.builder()
+                .user(user)
+                .build();
+    }
+
+    public static ManagerProfile buildManagerProfile(User user, String nationalId) {
+        return ManagerProfile.builder()
+                .user(user)
+                .nationalId(nationalId)
+                .build();
+    }
+
+    public AdminProfile buildAdminProfile(User user) {
+        return AdminProfile.builder()
+                .user(user)
+                .build();
+    }
+
 }
